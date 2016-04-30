7e75d5942ff4dbda6c364f50fdb0685660cb4c71
muukii <m@muukii.me>
2016-05-01 01:18:58 +0900

    Change bootstrap theme

--- container.jsx
+++ container.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ButtonToolbar } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 
 class Container extends React.Component {
   render() {
@@ -13,3 +15,30 @@ ReactDOM.render(
   <Container />,
   document.getElementById('container')
 )
+
+const buttonsInstance = (
+  <ButtonToolbar>
+    {/* Standard button */}
+    <Button>Default</Button>
+
+    {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+    <Button bsStyle="primary">Primary</Button>
+
+    {/* Indicates a successful or positive action */}
+    <Button bsStyle="success">Success</Button>
+
+    {/* Contextual button for informational alert messages */}
+    <Button bsStyle="info">Info</Button>
+
+    {/* Indicates caution should be taken with this action */}
+    <Button bsStyle="warning">Warning</Button>
+
+    {/* Indicates a dangerous or potentially negative action */}
+    <Button bsStyle="danger">Danger</Button>
+
+    {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+    <Button bsStyle="link">Link</Button>
+  </ButtonToolbar>
+);
+
+ReactDOM.render(buttonsInstance, document.getElementById('container'));
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,11 @@ module.exports = {
         query: {
           presets: ['es2015', 'react']
         }
-      }
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css?modules'],
+      },
     ]
   }
 }
