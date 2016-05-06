94f9a43c7bf7e60e0c50461c9a589ee6bf982415
muukii <m@muukii.me>
2016-05-07 01:52:19 +0900

    wip :D

--- typings/browser.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="browser/ambient/react-bootstrap/index.d.ts" />
-/// <reference path="browser/ambient/react-dom/index.d.ts" />
-/// <reference path="browser/ambient/react-router/index.d.ts" />
-/// <reference path="browser/ambient/react/index.d.ts" />
-/// <reference path="browser/definitions/react-router/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="main/ambient/react-bootstrap/index.d.ts" />
-/// <reference path="main/ambient/react-dom/index.d.ts" />
-/// <reference path="main/ambient/react-router/index.d.ts" />
-/// <reference path="main/ambient/react-router/history.d.ts" />
-/// <reference path="main/ambient/react/index.d.ts" />
